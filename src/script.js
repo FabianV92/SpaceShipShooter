31e5990845a1dd4ce0c7847359cdec1ef726169e
FabianV92 <71566212+FabianV92@users.noreply.github.com>
2021-03-09 22:18:11 +0100

    Added code

--- src/script.js
+++ src/script.js
@@ -1,6 +1,43 @@
-// Selecting elements
-const canvas = document.querySelector("canvas");
+"use strict";
+// Window on load function
 
-// Setting the height and width to the canvas
+// Selecting elements
+
+class model {
+}
+
+
+class view {
+
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    windowSettings = function () {
+        const bodyEl = document.querySelector("body");
+        const canvasEl = document.querySelector(".canvas");
+        bodyEl.style.cssText = "display: flex; justify-content: center;" +
+            " align-items: center; width: 100vw; height: 100vh";
+        bodyEl.style.backgroundColor = "black";
+        canvasEl.width = window.innerWidth = this.width;
+        canvasEl.height = window.innerHeight = this.height;
+    }
+}
+
+const test2 = new view(750, 500);
+test2.windowSettings();
+
+class controller {
+}
+
+class main {
+}
+
+
+
+
+
+
 
 
